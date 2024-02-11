3a2c0f06c11a7ce8971c2521f4b53ba20beac117
unitythecoder <unitythecodergit@gmail.com>
2024-02-11 16:00:22 +0100

    [+] Textures testing

--- spgame/game/main.cpp
+++ spgame/game/main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[]) {
     EngineInstance *instance = Arena::NewInstance(WIDTH, HEIGHT, TITLE);
+    instance->LoadTextures("gameroot/textures/textures.rmap");
     instance->LoadLevel("gameroot/levels/testing.arena");
     instance->GameLoop();
     return 0;
